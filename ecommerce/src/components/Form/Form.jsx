e796762f2fe5c078a3918a4c949ac15ed4a2aa92
Jessica Menendez <jessica.menendez@claro.com.ar>
2024-01-08 09:46:47 -0300

    se agrega codigo para reentrega

--- ecommerce/src/components/Form/Form.jsx
+++ ecommerce/src/components/Form/Form.jsx
@@ -32,15 +32,6 @@ export const Formulario = () => {
             Finalizar compra
           </Button>
       </Box>
-      
-    // <Box display={"flex"} flexDirection={"column"} width={400} component="form" onSubmit={handleForm}>
-    //   <Stack spacing={3}>
-    //     <TextField label={"Nombre"} value={name} onChange={(e) => setName(e.target.value)} />
-    //     <TextField label={"Email"} value={email} onChange={(e) => setEmail(e.target.value)} />
-    //     <Button type="submit" variant="contained" color="success">
-    //       Comprar
-    //     </Button>
-    //   </Stack>
-    // </Box>
+
   );
 };
